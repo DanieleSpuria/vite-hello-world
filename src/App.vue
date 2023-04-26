37c3f14964ba244258f6ab5ddb30546c811f33bb
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-04-26 14:08:20 +0200

    add AppTitle.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
-<template>
-
-</template>
 
 <script>
+import AppTitle from './components/AppTitle.vue'
 
+export default {
+  name: 'App',
+  components: {
+    AppTitle
+  }
+}
 </script>
 
+<template>
+  <div class="box">
+    <AppTitle />
+  </div>
+</template>
+
 <style scoped>
+  .box{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
 </style>
